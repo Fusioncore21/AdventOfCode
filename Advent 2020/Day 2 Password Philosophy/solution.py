33a8939318abfca16b9a3ae3f3d5e0ebf4141c50
Fusioncore21 <54691333+Fusioncore21@users.noreply.github.com>
2021-12-02 15:31:31 +0000

    Update solution.py

--- Advent 2020/Day 2 Password Philosophy/solution.py
+++ Advent 2020/Day 2 Password Philosophy/solution.py
@@ -1,6 +1,30 @@
 from csv import reader
+
+
+# Extract Input Data
 with open("input.csv","r") as data:
     pData = [i[0] for i in reader(data)]
 
+# Parse data for needs
+for index,inp in enumerate(pData):
+    inp = inp.split()
+    inp[0] = inp[0].split("-")
+    inp[1] = inp[1].removesuffix(":")
+    pData[index] = inp
 
-print(pData)
+def Part1(array):
+    validPasswords = 0
+    for policy in array:
+        print(policy)
+        countedLetters = int(policy[2].count(policy[1]))
+        min = int(policy[0][0])
+        max = int(policy[0][1])
+        if countedLetters >= min and countedLetters <= max:
+            validPasswords += 1
+    return validPasswords
+
+def Part2(array):
+    ...
+
+print(f"Part 1: {Part1(pData)}")
+print(f"Part 2: {Part2(pData)}")
