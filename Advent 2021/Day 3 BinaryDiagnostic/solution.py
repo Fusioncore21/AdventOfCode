1b4638bd2dd1c1c0e28166f2401707f9417735ee
Fusioncore21 <54691333+Fusioncore21@users.noreply.github.com>
2021-12-03 18:30:26 +0000

    Day 3 finished!!!

--- Advent 2021/Day 3 BinaryDiagnostic/solution.py
+++ Advent 2021/Day 3 BinaryDiagnostic/solution.py
@@ -1,13 +1,14 @@
 from csv import reader
+from typing import Counter
 
 def parse_data():
-    with open("testinput.csv","r") as f:
+    with open("input.csv","r") as f:
         return [i[0] for i in reader(f)]
 
-def part1(array):
+def part1(array: list):
     row,gammaRate,epsilonRate = 0,"",""
     # Count highest digit from each row
-    for i in range(0,12):
+    for i in range(12):
         counter = 0
         for string in array:
             if string[row] == "0": counter += 1
@@ -16,19 +17,42 @@ def part1(array):
         row += 1
     return int(epsilonRate,2) * int(gammaRate,2)
 
-def part2(O2Array,CO2Array):
-    for Row in range(0,12):
-        # O2 Finding Bit to Filter
-        if sum([int(i[Row]) for i in O2Array]) >= len(O2Array) // 2: O2FilterBit = "0"
-        else: O2FilterBit = "1"
-        #CO2 Finding Bit to Filter
-        if sum([int(i[Row]) for i in CO2Array]) <= len(CO2Array) // 2: CO2FilterBit = "1"
-        else: CO2FilterBit = "0"
-        # Filter Arrays
-        
+# My method that sucks due to having 2 ifs
+def part2a(O2Array: list, CO2Array: list):
+    for Row in range(12):
+        try:
+            O2Filter = Counter([i[Row] for i in O2Array]).most_common(2)
+            CO2Filter = Counter([i[Row] for i in CO2Array]).most_common(2)
+        except IndexError: continue
 
-def main(): 
-    #print(f"Part 1 Answer: {part1(parse_data())}")
-    print(f"Part 2 Answer: {part2(parse_data(),parse_data())}")
+        if len(O2Array) > 1:
+            if O2Filter[0][1] == O2Filter[1][1]:
+                O2Array = [i for i in O2Array if i[Row] == "1"]
+            else:
+                O2Array = list(filter(lambda a: a[Row] == O2Filter[0][0],O2Array))
+        if len(CO2Array) > 1:
+            if CO2Filter[0][1] == CO2Filter[1][1]:
+                CO2Array = [i for i in CO2Array if i[Row] == "0"]
+            else:
+                CO2Array = list(filter(lambda a: a[Row] == CO2Filter[-1][0],CO2Array))
+    return int(O2Array[0],2) * int(CO2Array[0],2)
 
+# Discord User's Xelf better method using more precise lambda functions
+def part2b(O2Array: list, CO2Array: list):
+    for Row in range(12):
+        O2Counter = Counter([i[Row] for i in O2Array]).most_common(2)
+        CO2Counter = Counter([i[Row] for i in CO2Array]).most_common(2)
+        O2Filter = lambda a: a[Row] == ('1' if len(O2Counter)>1 and O2Counter[0][1]==O2Counter[1][1] else O2Counter[0][0])
+        CO2Filter = lambda a: a[Row] == ('0' if len(CO2Counter)>1 and CO2Counter[0][1]==CO2Counter[1][1] else CO2Counter[-1][0])
+        O2Array = list(filter(O2Filter,O2Array)) or O2Array
+        CO2Array = list(filter(CO2Filter,CO2Array)) or CO2Array
+    return int(O2Array[0],2) * int(CO2Array[0],2)
+
+
+
+def main():
+    data = parse_data()
+    print(f"Part 1 Answer: {part1(data)}")
+    print(f"Part 2a Answer: {part2a(data, data)}")
+    print(f"Part 2b Answer: {part2b(data, data)}")
 main()
