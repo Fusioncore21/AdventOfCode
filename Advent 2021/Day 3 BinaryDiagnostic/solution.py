a723c802937f27ea9b80e0d60ccfd11b6cd89c6d
Fusioncore21 <54691333+Fusioncore21@users.noreply.github.com>
2021-12-03 15:07:04 +0000

    D:<<<<<

--- Advent 2021/Day 3 BinaryDiagnostic/solution.py
+++ Advent 2021/Day 3 BinaryDiagnostic/solution.py
@@ -1,7 +1,7 @@
 from csv import reader
 
 def parse_data():
-    with open("input.csv","r") as f:
+    with open("testinput.csv","r") as f:
         return [i[0] for i in reader(f)]
 
 def part1(array):
@@ -16,41 +16,19 @@ def part1(array):
         row += 1
     return int(epsilonRate,2) * int(gammaRate,2)
 
-def part2a(array: list):
-    Row = 0
-    for _ in range(0,12):
-        Sum = sum([int(i[Row]) for i in array])
-        ArrayLength = len(array) // 2
-        print(Sum, ArrayLength) 
-        if Sum > len(array) // 2: FilterBit = "0"
-        else: FilterBit = "1"
+def part2(O2Array,CO2Array):
+    for Row in range(0,12):
+        # O2 Finding Bit to Filter
+        if sum([int(i[Row]) for i in O2Array]) >= len(O2Array) // 2: O2FilterBit = "0"
+        else: O2FilterBit = "1"
+        #CO2 Finding Bit to Filter
+        if sum([int(i[Row]) for i in CO2Array]) <= len(CO2Array) // 2: CO2FilterBit = "1"
+        else: CO2FilterBit = "0"
+        # Filter Arrays
         
-        if len(array) > 2:
-            array = list(filter(lambda a: a[Row] != FilterBit, array))  
-        else:
-            return int(list(filter(lambda a: a[Row] == "1", array))[0],2)
-        Row += 1
-
-
-def part2b(array: list):
-    Row = 0
-    for _ in range(0,12):
-        Sum = sum([int(i[Row]) for i in array])
-        ArrayLength = len(array) // 2
-        print(Sum, ArrayLength) 
-        print(array)
-        if Sum > len(array) // 2: FilterBit = "1"
-        else: FilterBit = "0"
-        
-        if len(array) > 2:
-            array = list(filter(lambda a: a[Row] != FilterBit, array))  
-        else:
-            return list(filter(lambda a: a[Row] == "0", array))[0]
-        Row += 1
 
 def main(): 
     #print(f"Part 1 Answer: {part1(parse_data())}")
-    print(f"Part 2 Answer: {part2a(parse_data())}")
-    print(f"Part 2 Answer: {part2b(parse_data())}")
+    print(f"Part 2 Answer: {part2(parse_data(),parse_data())}")
 
 main()
