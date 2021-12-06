72c413cc87c546598ecfdb5a5340dec62cc2ea0b
Fusioncore21 <54691333+Fusioncore21@users.noreply.github.com>
2021-12-06 09:01:32 +0000

    :(

--- Advent 2021/Day 4 Giant Squid/solution.py
+++ Advent 2021/Day 4 Giant Squid/solution.py
@@ -1,9 +1,20 @@
 from csv import reader
-import numpy as np
+
+def save_data(array: list):
+    Counter = 0
+    with open("output.txt","w") as f:
+        for bingoCard in array:
+            for row in bingoCard:
+                f.write(f"{row}\n")
+                Counter+=1
+                if Counter%5==0: f.write("\n"); Counter = 0
+
+
+    return True
 
 def parse_data():
     toReturn = []
-    with open("testinput.csv","r") as d:
+    with open("input.csv","r") as d:
         data = [i for i in reader(d) if i]
         n = data.pop(0)
         data = [i[0].split() for i in data]
@@ -17,29 +28,16 @@ def part1(array: list, numbers: list):
             for index2, row in enumerate(bingoCard,0): 
                 row = list(filter(lambda a: a!=number, row))
                 # Check if the filter has caused a Bingo
-                print(number, array[2][0])
                 if not bool(row): return sum([sum(list(map(int,a))) for a in bingoCard]) * int(number)
                 # If the row isn't bingo'd, what about the columns?
                 array[index1][index2] = row
-
-def part1a(boards, draws):
-    board_indices = draws.argsort()[boards]
-    win_turn = np.minimum(board_indices.max(axis=1).min(axis=1), board_indices.max(axis=2).min(axis=1))
-    win_ind = win_turn.argmin()
-    first_win_turn = win_turn[win_ind]
-    winning_board = boards[win_ind].reshape(-1, 1)
-    winning_board[(winning_board == draws[:first_win_turn+1].reshape(1, -1)).any(axis=1)] = 0
-    lose_ind = win_turn.argmax()
-    last_win_turn = win_turn[lose_ind]
-    losing_board = boards[lose_ind].reshape(-1, 1)
-    losing_board[(losing_board == draws[:last_win_turn + 1].reshape(1, -1)).any(axis=1)] = 0
-    return winning_board.sum() * draws[first_win_turn], losing_board.sum() * draws[last_win_turn]
+            #save_data(array)
+            #input()
 
 def part2():
     ...
 
 def main():
     print(part1(parse_data()[0],parse_data()[1]))
-    print(part1a(parse_data()[0],parse_data()[1]))
 
 main()
