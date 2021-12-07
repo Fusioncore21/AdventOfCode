f421dc98bc59a2c4ac204f731f62000f8345385d
Fusioncore21 <54691333+Fusioncore21@users.noreply.github.com>
2021-12-07 12:13:19 +0000

    Day 7 complete

--- Advent 2021/Day 7 Whale Treachery/solution.py
+++ Advent 2021/Day 7 Whale Treachery/solution.py
@@ -1,7 +1,34 @@
 from csv import reader
-
+from statistics import mean
 def getData():
-    with open("testinput.csv","r") as f:
-        return list(map(int,list(reader(f))[0]))
+    with open("input.csv","r") as f:
+        return sorted(list(map(int,list(reader(f))[0])))
 
-print(getData().sort())
+def part1(array: list):
+    fuel_cost, fuel_target = 0, array[(len(array)//2)-1]
+    for crab in array:
+        fuel_cost += abs(fuel_target-crab)
+    return fuel_cost
+
+# Answer from Redditor NoisyFrequency
+def part2(array: list):
+    def cost(x):
+        return int(x * (x+1)/2)
+
+    def sum_cost(data,val):
+        return sum(cost(abs(elem-val)) for elem in data)
+
+    window = int(len(array)*.1)
+    m = round(mean(array))
+    minim = sum_cost(array,m)
+
+    for i in range(m-window,m+window):
+        if (temp := sum_cost(array,i)) < minim:
+            minim = temp
+
+    return minim
+
+def main():
+    print(f"Part 1: {part1(getData())}")
+    print(f"Part 2: {part2(getData())}")
+main()
