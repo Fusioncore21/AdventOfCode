724c680d15e901b0832ff61ae5cefd0ff6dad5e6
Fusioncore21 <54691333+Fusioncore21@users.noreply.github.com>
2021-12-08 23:07:45 +0000

    I tried

--- Advent 2021/Day 8 Digital Clocks/solution.py
+++ Advent 2021/Day 8 Digital Clocks/solution.py
@@ -1,22 +1,24 @@
 from csv import reader
 def getData():
-    with open("input.csv","r") as f:
+    with open("testinput.csv","r") as f:
         data = list(reader(f))
         for i,v in enumerate(data,0):
-            data[i] = [i.split() for i in v[0].split("|")]
+            data[i] = [i for i in v[0].split("|")]
     return data
         
 def part1(array: list):
     counter = 0
     for set in array:
         for item in set[1]:
-            if len(item) == 2 or len(item) == 3 or len(item) == 4 or len(item) == 7: counter+=1
-
+            if len(item) in {2,3,4,7}: counter+=1
+    
     return counter
 
-
+def part2(array: list):
+    ...
 
 def main():
-    print(part1(getData()))
+    print(getData())
+    #print(part1(getData()))
 
 main()
