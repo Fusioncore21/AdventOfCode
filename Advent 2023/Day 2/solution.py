0e0eb702af7ad213477d21857c2626926cb88452
Fusioncore21 <54691333+Fusioncore21@users.noreply.github.com>
2023-12-02 17:26:48 +0000

    wip

--- Advent 2023/Day 2/solution.py
+++ Advent 2023/Day 2/solution.py
@@ -1,1 +1,12 @@
+def part1(file):
+    for line in file:
+        pass
 
+def part2(file):
+    pass
+
+with open("./Advent 2023/Day 2/input.txt", "r") as raw:
+    file = [line.rstrip("\n") for line in raw.readlines()]
+
+print(part1(file))
+print(part2(file))
