cf3b903c5a57e3709a0ddc6ae5c378700753ae0f
Sam <sphilipsminecraft@gmail.com>
2023-12-05 15:54:58 +0000

    updates

--- Advent 2023/Day 4 Scratchcards/solution.py
+++ Advent 2023/Day 4 Scratchcards/solution.py
@@ -1,25 +1,22 @@
-import re
-
-
 def part1(file):
-    file = [row.split(": ")[1] for row in file] # Remove Game X: 
+    file = [row.split(":")[1] for row in file] # Remove Card X: 
     total = 0
     for row in file:
-        win,card = row.split(" | ")
-        win = win.replace("  ", " ").replace(" ", " | ") #Remove double spacing, then replaces spaces with |
-
-        result = list(re.finditer(win,card))
-        length = len(result)
-        print(f"{win} string | {card} -> {[i.group() for i in result]}")
-
-        if length > 0:
-            total += 2**(length-1)
-
+        win,card = row.split("|")
+        win_numbers = set(win.split()) & set(card.split())
+        if (length:=len(win_numbers)) > 0:
+           total += 2**(length-1)
     return total
 
+def part1_1line(file):
+    return sum([(2**(total:=len(set(row.split("|")[0].split()) & set(row.split("|")[1].split()))-1)) for row in [row.split(":")[1] for row in file] if total>0])
+
+class Card():
+    def __init__(self, details):
+        self.
 
 def part2(file):
-    pass
+    
 
 with open("./Advent 2023/Day 4 Scratchcards/input.txt","r") as raw:
     File = [row.rstrip("\n") for row in raw.readlines()]
